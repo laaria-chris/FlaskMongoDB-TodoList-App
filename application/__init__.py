0891f502dc0fa75fcc4986a3ca912df0a5e1b790
cipher <laariachris@gmail.com>
2024-05-12 20:00:37 +0300

    MongoDB connection timeout increased from 5000ms to 10000ms
    Binary files in the __pycache__ directory have also been modified, but the specific changes cannot be determined from the provided information.

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Setup MongoDB connection
 app.config["SECRET_KEY"] = "c15bf8093aa07284f719e278c3dd8cc8dbdc46c3"
 conn = "mongodb+srv://laaria:laaria9251@cluster0.g9lsxqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = pymongo.MongoClient(conn, serverSelectionTimeoutMS=5000)
+client = pymongo.MongoClient(conn, serverSelectionTimeoutMS=10000)
 db = client.db
 
 # Import routes
